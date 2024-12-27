5ab784277b32d49c16d0f6e6f51b9e023ac86102
bron <thinh.nt93@gmail.com>
2024-12-27 20:07:58 +0700

    update banner and index.html

--- google_map_field/__manifest__.py
+++ google_map_field/__manifest__.py
@@ -23,7 +23,7 @@
         ],
     },
     'images': [
-        'static/description/banner.jpg',
+        'static/description/banner.png',
     ],
     'installable': True,
     'auto_install': False,
